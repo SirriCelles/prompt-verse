c6113ef1647fcc6e717ba97c231b88b5f9442afd
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-07-20 04:09:29 +0100

    implement manage UI functionality

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -11,20 +11,20 @@ export const GET = async (req, { params }) => {
 
     if(!prompt) return new Response('Prompt not found', {status: 404});
 
-    return new Response(JSON.stringify(prompts), {status: 200});
+    return new Response(JSON.stringify(prompt), {status: 200});
   } catch (error) {
-    return new Response('Failed to fetch prompts', {status: 500});
+    return new Response('Failed to fetch prompt', {status: 500});
   }
 }
 
 export const PATCH = async (req, { params }) => {
-  const { prompt, tag } = req.json();
+  const { prompt, tag } = await req.json();
 
   try {
     await connectToDB();
     const existingPrompt  = await Prompt.findById(params.id);
 
-    if(!prompt) return new Response('Prompt not found', {status: 404});
+    if(!existingPrompt) return new Response('Prompt not found', {status: 404});
 
     existingPrompt.prompt = prompt;
     existingPrompt.tag = tag;
@@ -34,6 +34,7 @@ export const PATCH = async (req, { params }) => {
     return new Response(JSON.stringify(existingPrompt), { status: 200});
 
   } catch (error) {
+    console.error(error);
     return new Response('Failed to update prompt', {status: 500});
   }
 }
