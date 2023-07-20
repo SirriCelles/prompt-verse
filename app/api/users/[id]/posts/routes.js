dd2be4cbd78acf19f29d34821feb016ced9838eb
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2023-07-20 02:01:45 +0100

    adding user profile

--- app/api/users/[id]/posts/routes.js
+++ app/api/users/[id]/posts/routes.js
@@ -1,11 +1,15 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const GET = async (req) => {
+
+export const GET = async (req, { params }) => {
+  const { id: userId } = params;
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate('creator');
+    const prompts = await Prompt.find({
+      creator: userId,
+    }).populate('creator');
     return new Response(JSON.stringify(prompts), {status: 200});
   } catch (error) {
     return new Response('Failed to fetch prompts', {status: 500});
